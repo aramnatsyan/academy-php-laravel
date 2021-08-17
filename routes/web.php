b56cb020be8ee6a63f3a87ee71f24f7c616d3504
aramnatsyan <aramnatsyan018@gmail.com>
2021-08-17 11:58:49 +0400

    Auth implementation

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('home', function () {
+    return view('home');
+});
+
+Route::get('login', function () {
+    return view('Login');
+});
+
+Route::get('register', function () {
+    return view('register');
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
